14e5f9ef7b431a74c4c8dfbca9c5724aaa9bd5d8
UssuyNeumann <luis.eduardo.un@hotmail.com>
2021-10-01 18:55:37 -0300

    script teste include

--- teste.php
+++ teste.php
@@ -1,8 +1,10 @@
 <?php
 
+include_once('\workspace\calendario-msg\script.php');
+
 $dia = new Mensagem();
 
-
-$data = '01-10-2021';
+$data = '29-09-2021';
 
 echo $dia->verDia($data, new MensagemStrategy());
+
